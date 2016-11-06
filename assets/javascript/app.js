1b0ec83859bec1403c52f3a4dafbcf307c1dcb2e
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-06 09:25:22 -0600

    Adds some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var feels = ['Happy Dance', 'Yas!', 'Tired', 'Eye Roll', 'Confused', 'Angry', 'Scared'];
+    var feels = ['Happy Dance', 'So Tired', 'Yas, Kween!', 'I Can\'t Even', 'Forgot How to Person', 'Huh?', 'Angry', 'Scared', 'Forever Alone', 'Mic Drop', 'Blerg'];
 
     function getTheFeels() {
 
@@ -15,33 +15,32 @@ $(document).ready(function() {
             .done(function(response) {
 
                 for (var i = 0; i < response.data.length; i++) {
-                    $('#GIF-area').prepend('<p>Rating: ' + response.data[i].rating + '</p>');
+
                     $('#GIF-area').prepend("<img src='" + response.data[i].images.fixed_height_still.url + "'" + "data-still='" + response.data[i].images.fixed_height_still.url + "'" + "data-animate='" + response.data[i].images.fixed_height.url + "'>");
+                    $('#GIF-area').prepend('<p>Rating: ' + response.data[i].rating + '</p>');
                     $('img').attr("data-state", "still");
                     $('img').addClass('gif');
                 }
             })
     };
 
-
-
     function renderButtons() {
 
-        // Deletes the feels prior to adding new movies (this is necessary otherwise you will have repeat buttons)
+        // Prevents repeat buttons)
 
         $('#button-area').empty();
 
-        // Loops through the array of movies
+        // Loops through the array of feels
+
         for (var i = 0; i < feels.length; i++) {
 
             // Then dynamicaly generates buttons for each feel in the array
 
-            // Note the jQUery syntax here... 
-            var button = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-            button.addClass('feel'); // Added a class 
-            button.attr('data-search', feels[i]); // Added a data-attribute
-            button.text(feels[i]); // Provided the initial button text
-            $('#button-area').append(button); // Added the button to the HTML
+            var button = $('<button>');
+            button.addClass('feel'); // Adds a class 
+            button.attr('data-search', feels[i]); // Adds a data-attribute
+            button.text(feels[i]); // Provides the initial button text
+            $('#button-area').append(button); // Adds the button to the HTML
         }
     }
 
@@ -50,45 +49,24 @@ $(document).ready(function() {
         // This line of code will grab the input from the textbox
         var feel = $('#feels-input').val().trim();
 
-        // The movie from the textbox is then added to our array
+        // The feel from the textbox is then added to the feels array
         feels.push(feel);
 
-        // Our array then runs which handles the processing of our movie array
+        // Our array then runs, which handles the processing of the array
         renderButtons();
 
-        // We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+        // This allows users to hit "enter" instead of clicking on the search button and stay on the page
         return false;
     })
 
     $(this).on('click', '.feel', getTheFeels);
 
-
-    // ========================================================
-
-    // This calls the renderButtons() function
     renderButtons();
 
     $(this).on('click', '.gif', function() {
-        //STEP ONE: study the html above. Look at all the data attributes. Run the file in the browser. Look at the images. After you fill in steps 1 and 2 you'll be able to pause gifs from giphy.
 
-        //STEP TWO: make a variable named state and then reference the button's data-state into it. Do not use .data('state'). It won't work the way we expect.
-
-        //---------------FILL IN CODE HERE FOR STEP TWO----------------------------
         var state = $(this).attr('data-state');
 
-
-        //----------------------------------------------------
-
-        /*STEP THREE:
-         * if variable state is equal to 'still' then
-         * update the src attribute of this image that you clicked on to what data-animate is equal to for this image
-         * and update the data-state attribute to 'animate'
-         * if state does not equal 'still' then
-         * update the src attribute of this image that you clicked on to what data-still is equal to for this image
-         * and update the data-state attribute to 'still'
-         */
-
-        //---------------FILL IN CODE HERE FOR STEP THREE----------------------------
         if (state == 'still') {
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate');
@@ -96,9 +74,7 @@ $(document).ready(function() {
             $(this).attr('src', $(this).data('still'));
             $(this).attr('data-state', 'still');
         }
-        //----------------------------------------------------
 
-        //STEP FOUR: open the file in the browser and click on the images. Then click again to pause.
     });
 
 });
